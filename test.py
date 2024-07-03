a6d610dab2c2e7a4688a9e2aad1ca748657fa508
n1xx1n <ndinevski5@gmail.com>
2024-07-03 22:00:31 +0200

    Add tests for facebook campaign and adset

--- test.py
+++ test.py
@@ -20,8 +20,8 @@ def print_json(message):
 def main():
     load_dotenv()
 
-    fb_token = os.getenv("FB_TOKEN")
-    fb_account_ids = ast.literal_eval(os.getenv("FB_ACCOUNT_IDS"))
+    fb_token = "EAANY2CrI0jUBO80TYU3COJfdSOQw0POgUuTmg69nWSt1cFwqfyjqgFiOXoCIgFXtcYvren482XwZC5RbHPZAktZByJomwzQpEZC5drDNq0V1tg5C23emycb7oaHECFeybQAiCCq3IxdmPJzEbeMCNQpBCqr8djUEFiZCxkgmHYcfPHUsjw8yuLHJwIOabrF4waUWXQ6WW"
+    fb_account_ids = ["act_366883153087738"]
     tt_token = os.getenv("TT_TOKEN")
     tt_account_ids = ast.literal_eval(os.getenv("TT_ACCOUNT_IDS"))
     tt_app_id = os.getenv("TT_APP_ID")
@@ -83,9 +83,36 @@ def main():
     #     user_access_token=fb_token
     # ))
 
+    # print_json(facebook_importer.create_campaign(
+    #     user_access_token=fb_token,
+    #     ad_account_id=fb_account_ids[0],
+    #     campaign_details={
+    #        'name': 'My Campaign 2',
+    #        'objective': 'OUTCOME_TRAFFIC',
+    #        'status':'PAUSED',
+    #        'special_ad_categories': ['NONE']
+    #      }
+    # ))
+
     # print_json(facebook_exporter.get_campaigns_details(
     #     user_access_token=fb_token,
     #     account_ids=fb_account_ids,
+    # ))
+
+    # print_json(facebook_importer.create_ad_set(
+    #     user_access_token=fb_token,
+    #     ad_account_id=fb_account_ids[0],
+    #     adset_details={
+    #         'name': 'My AdSet 2',
+    #         'optimization_goal': 'IMPRESSIONS',
+    #         'billing_event': 'IMPRESSIONS',
+    #         'bid_amount': 20,
+    #         'promoted_object': {},
+    #         'daily_budget': 1000,
+    #         'campaign_id': ['120330000915611219'],
+    #         'targeting': {'geo_locations':{'countries':['US']}},
+    #         'status': 'PAUSED',
+    #     }
     # ))
 
     # print_json(facebook_exporter.get_adsets_details(
